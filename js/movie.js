4cc9ffba59813aee7c2fb539a8b0e1669b18a4d7
kim <yellsdesign@naver.com>
2024-11-30 14:20:47 +0900

    modify

--- js/movie.js
+++ js/movie.js
@@ -1,43 +1,52 @@
 movieList();
 
-
-
-
- 
 async function movieList(){
     let date = new Date();
-    let getDate = date.getFullYear().toString().concat(date.getMonth()+1, date.getDate()-1);
+    let getDate = (date.getFullYear()-1).toString().concat(date.getMonth()+1, date.getDate()-1);
     kobis(getDate)
         .then((result) => {
+
             let BoxOfficeList = result.boxOfficeResult.dailyBoxOfficeList;
             let output = ``;
+            let posters = [];
             BoxOfficeList.forEach((element) => {
-            output += `
-                <div class="swiper-slide">
-                    <div class="thumb">
-                        <img src="img/88738_320.jpg" alt="">
-                        <div class="num">${element.rank}</div>
-                        <div class="dim">
-                            <a href="">상세보기</a>
-                            <a href="">예매하기</a>
-                        </div>
-                    </div>
-                    <div>
-                        <strong>${element.movieNm}</strong>
-                        <div>
-                            <span>98%</span>
-                            <span>관객수 ${element.audiCnt}</span>
-                        </div>
-                    </div>
-                </div>
-            `;
-            document.querySelector('.slide_area.movie .swiper-wrapper').innerHTML = output;
+                posters.push(getPoster(element.movieNm));
+                
            });
+
+           Promise.all(posters)
+                .then((poster)=>{            
+                    BoxOfficeList.forEach((element,index) => {
+                        output += `
+                            <div class="swiper-slide">
+                                <div class="thumb">
+                                    <img src="${poster[index]}" alt="">
+                                    <div class="num">${element.rank}</div>
+                                    <div class="dim">
+                                        <a href="">상세보기</a>
+                                        <a href="">예매하기</a>
+                                    </div>
+                                </div>
+                                <div>
+                                    <strong>${element.movieNm}</strong>
+                                    <div>
+                                        <span>누적관객수 ${element.audiCnt}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        `;
+                        document.querySelector('.slide_area.movie .swiper-wrapper').innerHTML = output;
+                    });
+                })
+                .catch((error) => console.log('error'));
         })
         .catch((error) => console.log('error'));
+    
 }
 
-
+async function getPoster(movieNm){
+    return await kmdb(movieNm)
+}
 
 
 
@@ -51,14 +60,13 @@ async function kobis(targetDt){
     return jsonData;
 }
 
-// kmdb
+// kmdb api
 async function kmdb(title){
     let key = `MO075W76WVQ09UK49059`;
-    let url =  `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_xml2.jsp?`;
+    let url =  `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?`;
         url += `collection=kmdb_new2&detail=Y&title=${title}&ServiceKey=${key}`;
-    let kmdb = await fetch(url); 
-    console.log(url);
-    
+    let kmdb = await fetch(url);
     let jsonData = await kmdb.json();
-    // return jsonData;
+    let posters = jsonData.Data[0].Result[0].posters.split('|');
+    return posters[0];
 }
